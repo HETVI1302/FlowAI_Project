bd09a333a3441301a33bd2b31423511723d72597
Hetvi rana <hetvirana094@gmail.com>
2026-07-30 20:37:13 +0530

    Delete accounts directory
    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'name', 'role', 'is_active', 'created_at')
-    list_filter = ('role', 'is_active', 'is_staff')
-    search_fields = ('email', 'name')
-    ordering = ('-created_at',)

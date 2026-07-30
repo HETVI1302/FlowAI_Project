79e47caba523c8e28e78c74586f9e430ffa78503
Hetvi rana <hetvirana094@gmail.com>
2026-07-30 20:37:32 +0530

    Delete analytics directory
    Delete analytics directory

--- analytics/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Emission
-
-@admin.register(Emission)
-class EmissionAdmin(admin.ModelAdmin):
-    list_display = ('intersection', 'carbon_emission', 'fuel_consumption', 'timestamp')
-    list_filter = ('intersection', 'timestamp')
-    ordering = ('-timestamp',)

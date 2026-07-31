5651ae10716f7a44c30636672d50141c05f25206
Hetvi rana <hetvirana094@gmail.com>
2026-07-31 14:52:51 +0530

    Delete signals_app directory
    Delete signals_app directory

--- signals_app/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-
-from . import consumers
-
-# Included from flowai_core/asgi.py into the project-wide websocket_urlpatterns.
-websocket_urlpatterns = [
-    re_path(r'^ws/signals/overview/$', consumers.SignalsOverviewConsumer.as_asgi()),
-]
